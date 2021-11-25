5382e1b854ad4dfb37300d2713ffdc1e7c4d8b3f
Moises Cruz <moisesbraga223@gmail.com>
2021-11-25 17:10:40 -0300

    Exercícios atualizados!

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var v = 58;
+console.log(`A velocidade do seu carro é ${v}km/h`);
+
+if (v > 60) {
+    console.log(`Seu carro passou do limite de velocidade.`);
+}
+
+console.log(`Dirija sempre com sinto de segurança!`);
---
+++ ex009.js
@@ -0,0 +1,8 @@
+var p = 'EUA';
+if (p == 'Brasil') {
+    console.log('Você é brasileiro!');
+
+} else {
+    console.log('Você é estrangeiro!');
+
+}
