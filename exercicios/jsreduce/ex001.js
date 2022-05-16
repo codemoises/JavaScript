77c3829e194048af7081ee0209d27411ea6ba217
Moises Cruz <moisesbraga223@gmail.com>
2022-05-16 16:10:20 -0300

    Atualizado.

---
+++ exercicios/jsreduce/ex001.js
@@ -0,0 +1,15 @@
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 105 },
+    { nome: 'Letícia', idade: 10 },
+    { nome: 'Rosana', idade: 64 },
+    { nome: 'Wallace', idade: 1000 },
+];
+
+const maisVelha = pessoas.reduce(function (acumulador, valor) {
+    if (acumulador.idade > valor.idade) return acumulador;
+    return valor;
+});
+
+console.log(maisVelha);
